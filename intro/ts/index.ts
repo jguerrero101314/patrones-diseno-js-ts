348d9c5bc76f6cf09d0e42815310b7a4cecff5e4
jguerrero101314 <joelg1014@hotmail.com>
2022-07-03 15:01:27 -0500

    interfaces en ts

--- intro/ts/index.ts
+++ intro/ts/index.ts
@@ -15,14 +15,25 @@ class Drink {
 const drink = new Drink('agua');
 console.log(drink.info());
 
+// interface
+interface Product {
+  price: number;
+  getPrice(): string;
+}
+
 // Herencia
-
-class Beer extends Drink {
+class Beer extends Drink implements Product {
   private alcohol: number;
+  price: number;
 
-  constructor(name: string, alcohol: number) {
+  constructor(name: string, alcohol: number, price: number) {
     super(name);
     this.alcohol = alcohol;
+    this.price = price;
+  }
+
+  getPrice(): string {
+    return 'El precio del alcohol es de: $ ' + this.price;
   }
 
   info(): string {
@@ -30,5 +41,33 @@ class Beer extends Drink {
   }
 }
 
-const beer = new Beer('erdinger ', 8.5);
+// implementacion de interface
+class Snack implements Product {
+  price: number;
+  name: string;
+
+  constructor(name: string, price: number) {
+    this.name = name;
+    this.price = price;
+  }
+
+  getPrice(): string {
+    return 'El precio del snack es de: $ ' + this.price;
+  }
+}
+
+const beer = new Beer('erdinger ', 8.5, 100);
 console.log(beer.info());
+const products: Product[] = [
+  new Beer('corona', 4.6, 1),
+  new Snack('papas', 4.6),
+  new Beer('heineken', 5, 1.3),
+];
+
+function getPrice(items: Product[]) {
+  for (const item of items) {
+    console.log(item.getPrice());
+  }
+}
+
+getPrice(products);
