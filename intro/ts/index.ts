f8f9620be12b3ca5482cc90a371547600fc27460
jguerrero101314 <joelg1014@hotmail.com>
2022-07-03 14:41:33 -0500

    herencia ts

--- intro/ts/index.ts
+++ intro/ts/index.ts
@@ -14,3 +14,21 @@ class Drink {
 
 const drink = new Drink('agua');
 console.log(drink.info());
+
+// Herencia
+
+class Beer extends Drink {
+  private alcohol: number;
+
+  constructor(name: string, alcohol: number) {
+    super(name);
+    this.alcohol = alcohol;
+  }
+
+  info(): string {
+    return super.info() + ' ' + this.alcohol;
+  }
+}
+
+const beer = new Beer('erdinger ', 8.5);
+console.log(beer.info());
