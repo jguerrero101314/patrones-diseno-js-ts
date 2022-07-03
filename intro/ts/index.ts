2354d66a356ff4e10ed739951c25a97a2a05cf2f
jguerrero101314 <joelg1014@hotmail.com>
2022-07-03 14:37:50 -0500

    POO en ts

--- intro/ts/index.ts
+++ intro/ts/index.ts
@@ -1,3 +1,16 @@
-function suma(a: number, b: number) {
-  return 3;
+console.log('Hola Mundo');
+
+class Drink {
+  private name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  info(): string {
+    return this.name;
+  }
 }
+
+const drink = new Drink('agua');
+console.log(drink.info());
