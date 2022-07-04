1904a6597ac194df967ff03bc014f2aca8fbc38c
jguerrero101314 <joelg1014@hotmail.com>
2022-07-03 21:20:29 -0500

    singleton js

--- singleton/singleton.js
+++ singleton/singleton.js
@@ -1,7 +1,28 @@
 class Singleton {
+  static getInstance() {
+    return Singleton.instance;
+  }
+
   constructor() {
+    // console.log('entrando a constructor');
+
+    this.random = Math.random();
+
     if (Singleton.instance) {
+      // console.log('ya existe');
       return Singleton.instance;
     }
+
+    // console.log('no existe y se crea');
+    Singleton.instance = this;
   }
 }
+
+const singleton = new Singleton();
+console.log(singleton.random);
+const singleton2 = new Singleton();
+console.log(singleton2.random);
+console.log(singleton === singleton2);
+const singleton3 = Singleton.getInstance();
+console.log(singleton3.random);
+console.log(singleton3 === singleton2);
