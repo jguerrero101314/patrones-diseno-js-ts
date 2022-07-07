66132968e2d0f7c44032fa3016f3754672ea595e
jguerrero101314 <joelg1014@hotmail.com>
2022-07-06 22:35:10 -0500

    singleton

--- singleton/singleton.js
+++ singleton/singleton.js
@@ -26,3 +26,46 @@ console.log(singleton === singleton2);
 const singleton3 = Singleton.getInstance();
 console.log(singleton3.random);
 console.log(singleton3 === singleton2);
+console.log('***************************************************');
+
+class WeekDays {
+  daysEs = [
+    'Lunes',
+    'Martes',
+    'Miercoles',
+    'Jueves',
+    'Viernes',
+    'Sabado',
+    'Domingo',
+  ];
+
+  daysEn = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ];
+
+  constructor(lang) {
+    this.lang = lang;
+
+    if (WeekDays.instance) {
+      return WeekDays.instance;
+    }
+
+    WeekDays.instance = this;
+  }
+
+  getDays() {
+    return this.lang === 'es' ? this.daysEs : this.daysEn;
+  }
+}
+
+const weekDays = new WeekDays('en');
+const weekDays2 = new WeekDays();
+
+console.log('weekDays: ', weekDays.getDays());
+console.log('weekDays2: ', weekDays2.getDays());
